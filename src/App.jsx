e7f40e2e31162a207b1b093849b4efb48a950b0b
okch0310 <okch0310@naver.com>
2024-04-10 23:35:47 +0900

    테스트 환경 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { decrease, increase } from "./utils";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [greet, setGreet] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/greet").then((res) => {
+      setGreet(res.data?.message || "");
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="card">
+      <div>{count}</div>
+      <button onClick={() => setCount((count) => increase(count))}>증가</button>
+      <button onClick={() => setCount((count) => decrease(count))}>감소</button>
+      {greet && <p data-testid="greet">{greet}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
